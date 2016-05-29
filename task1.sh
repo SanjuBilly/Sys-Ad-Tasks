713a9cf60d23e31a7c935aa59341da9cf658bf4a
SanjuBilly <sanjana22021998@gmail.com>
2016-05-29 18:22:56 +0530

    Create Task1

---
+++ task1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#to be run as root user
+echo Name of the directory ?
+read dir
+mkdir $dir
+cd $dir
+touch file{1..100}.txt
+truncate -s 10k file{1..100}.txt
+
+ for n in {1..100}; do
+    cat /dev/urandom | tr -dc A-Za-z0-9 | fold -w 32 | head -c 16 > file$n.txt 
+  done 
+
+chmod 444 -R file{1..100}.txt
+touch -d "2 days ago" file{1..100}.txt
+echo completed 
